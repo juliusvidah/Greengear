507e591a7053432b35af4b299cc743d042ff8b53
stephrett <stephenpanret1@gmail.com>
2024-11-19 14:07:03 +0100

    auth mobile view added

--- src/App.jsx
+++ src/App.jsx
@@ -10,8 +10,8 @@ function App() {
 
   return (
     <>
-      <SignIn/>
-      {/* <SignUp/> */}
+      {/* <SignIn/> */}
+      <SignUp/>
       {/* <PasswordResetForm/> */}
       {/* <EmailVerification/> */}
     </>
